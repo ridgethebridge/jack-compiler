1ac824a39063bd8d5b71792723cfd23cf078d784
tombour <ridgerouzier@gmail.com>
2025-07-17 05:18:51 -0400

    removed comment

--- src/syntax_analyzer.py
+++ src/syntax_analyzer.py
@@ -5,25 +5,6 @@ from sys import argv
 from xml_generator import *
 import os
 def main():
-    '''
-    if len(argv) < 2:
-        print("must supply either file or directory!")
-        exit()
-
-    file = argv[1]
-    if os.path.isdir(file):
-        file = os.listdir(file)
-    else:
-        file = [file]
-    for f in file:
-        if ".jack" in f:
-            lexer = Jack_Lexer(f)
-            lexer.lex_class_def()
-            if lexer.error_log != "":
-                print(lexer.error_log)
-                exit()
-            lexer.dump_xml_to_file(f.replace(".jack",".xml"))
-            '''
     if len(argv) < 2:
         print("must supply either file or directory!")
         exit()
